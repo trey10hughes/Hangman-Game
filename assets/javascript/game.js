5b9c74f95164770f74a425c8a20ce8bb8ffeda57
Theodore hughes <theodorehughes@Theodores-MacBook-Pro.local>
2017-11-18 00:57:00 -0600

    game now completely functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,16 +4,30 @@ var game = {
   chosenWord: "",
   displayWord: [],
   guessedLetter: "",
-  numBlanks: 0,
+  numBlanks: 99,
+  letterFound: false,
+  numWins: 0,
+  guessedLettersArchive: [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "], 
+  guessCount: 0, 
+  guessedBefore: false,
 
   chooseWord: function() {
+
     var randNum = Math.floor(Math.random() * 23);
     console.log("random number: " + randNum);
 
     this.chosenWord = this.characters[randNum];
     console.log(this.chosenWord);
 
+    this.numBlanks = this.chosenWord.length;
+    console.log("number of blanks to fill: " + this.numBlanks);
+
+    var stockCountDisplay = document.getElementById('stockCount');
+    console.log("stocks: " + this.stocks);
+    stockCountDisplay.innerHTML = this.stocks;
+
     var displayArea = document.getElementById('display-word-area');
+    displayArea.innerHTML = ""
 
     for(var i = 0; i < this.chosenWord.length; i++){
       this.displayWord[i] = "_";
@@ -32,19 +46,106 @@ var game = {
      
 
   guessLetter: function(x) {
-    // <input type="text" onkeyup="function for taking input and assigning to variable">
+    console.log("guess letter function called");
 
-    for(var i = 0; i < this.chosenWord.length; i++){
-      console.log(x);
-      if (x === this.chosenWord.charAt(i)) {
-        var displayCharacter = document.getElementsByClassName("character-display");
-        displayCharacter[i].innerHTML = x;
-      }
+    this.guessedBefore = false;
+    
+    for (var i = 0; i < this.guessedLettersArchive.length; i++){
+        if (this.guessedLettersArchive[i] === x){
+          this.guessedBefore = true;
+        }  
     }
-  }, //takes the letter you entered and runs through chosenWord using char.at
+
+    if (this.guessedBefore === true) {
+        alert("you already guessed this letter");
+    }
+    if (this.guessedBefore === false) {
+      this.guessedLettersArchive[this.guessCount] = x;
+      this.guessCount ++;
+
+      this.letterFound = false;
+      for (var i = 0; i < this.chosenWord.length; i++){
+      
+        if (x === this.chosenWord.charAt(i)) { // if what you input is in the word
+          var displayCharacter = document.getElementsByClassName("character-display");
+          displayCharacter[i].innerHTML = x;
+          this.numBlanks --;
+          console.log("number of letters left to find: " + this.numBlanks);
+           this.letterFound = true; //notating that it was in the word
+        }      
+      }
+      if (this.letterFound === false) {
+        var guessDisplay = document.getElementById('letters-guessed');
+        guessDisplay.innerHTML += x + " ";
+        console.log("not correct, you entered: " + x);
+
+        //lose a stock on the display
+        var stockCountDisplay = document.getElementById('stockCount');
+        this.stocks --;
+        console.log("stocks: " + this.stocks);
+        stockCountDisplay.innerHTML = this.stocks;
+      }
+
+    }   
+    }, //end of guessLetter
+  
+  
+   //takes the letter you entered and runs through chosenWord using char.at
      //checks for the letters that do match
      //letters that match will replace underscores in the display
      //if the letter doesn't match, take away 1 stock, add their input to letters guessed area
+
+  checkForWinLoss: function() {
+    if (this.stocks < 1) {
+      alert("you lost all your stocks! Press ok for new game.");
+      console.log("stocks left: " + this.stocks);
+
+      var stockCountDisplay = document.getElementById('stockCount');
+      this.stocks = 4;
+      stockCountDisplay.innerHTML = this.stocks; //resets stock count to 4 and updates display
+
+      var guessDisplay = document.getElementById('letters-guessed');
+      guessDisplay.innerHTML = ""; //clears out letters guessed
+
+      this.numBlanks = 99; //resets numBlanks to 99
+
+      this.guessCount = 0;//reset guesscount to 0
+      
+      for (var i = 0; i < this.guessedLettersArchive.length; i++){
+        this.guessedLettersArchive[i] = " ";
+      }//resets guessed letter archive back to blank
+
+      this.chooseWord();
+
+    }
+    if (this.numBlanks < 1) {
+      this.numWins ++;
+      console.log("wins: " + this.numWins);
+      alert("you win! Press ok for new game.");
+      this.stocks = 4;
+      this.numBlanks = 99;
+
+      var stockCountDisplay = document.getElementById('stockCount');
+      this.stocks = 4;
+      stockCountDisplay.innerHTML = this.stocks; //resets stock count to 4 and updates display
+
+      var guessDisplay = document.getElementById('letters-guessed');
+      guessDisplay.innerHTML = ""; //clears out letters guessed
+
+      this.numBlanks = 99; //resets numBlanks to 99
+
+      var winCountDisplay = document.getElementById('numberOfWins');
+      winCountDisplay.innerHTML = "Wins: " + this.numWins;
+
+      this.guessCount = 0;//reset guesscount to 0
+      for (var i = 0; i < this.guessedLettersArchive.length; i++){
+        this.guessedLettersArchive[i] = " ";
+      }//resets guessed letter archive back to blank
+      this.chooseWord();
+    }
+  }//end of check for win losss   
+
+  //function to reset stocks to 4, clear letters guessed, and numBlanks to 99.
 
   }; //end of object
   
@@ -52,26 +153,27 @@ var game = {
 
 game.chooseWord();
 
-document.onkeyup = function(event) {
-      var letter = String.fromCharCode(event.key).toLowerCase();
-      // console.log(choice);
-      if (event.key === "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"){
+document.onkeyup = function(evt) {
+
+  evt = evt || window.event;
+  var charCode = evt.which || evt.keyCode;
+  var charStr = String.fromCharCode(charCode);
+    if (/[a-z]/i.test(charStr)) {
+
       game.guessLetter(event.key);
-      }
-      else{
-        alert("incorrect input");
-      }
+      game.checkForWinLoss();
 
-      };
+    }
+};
+
+//error message for inputting something you've already put before
+//put every input into an array
+//check the array every time you input something new
+//if the item already exists in the array, alert "you already guessed this"
 
 
 
-//take in input to start the game, that input will run the function to choose a word
 
-
-//take in input again, checks to see if it is correct
-//if it is correct it changes it to lowercase and assigns it to a variable
-//pass that variable in to the functions where needed
 
 
 
